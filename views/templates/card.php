9deb8159662e8b76c93d4c5172ba3cdb55238dba
Alicia Bazin <bazin.alicia88@gmail.com>
2023-09-12 16:18:24 +0200

    integration home pages

--- views/templates/card.php
+++ views/templates/card.php
@@ -1,14 +1,14 @@
-<a href="<?php ?>">
+<a href="<?= $link ?>" target="_blank">
     <div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="<?php ?>" class="img-fluid rounded-start" alt="<?php  ?>">
+                <img src="<?= $imageUrl ?>" class="img-fluid rounded-start" alt="<?= 'image' . $title ?>">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title"><?php ?></h5>
-                    <p class="card-text"><?php ?></p>
-                    <p class="card-text"><small class="text-body-secondary"><?php ?></small></p>
+                    <h5 class="card-title"><?= $title ?></h5>
+                    <p class="card-text"><?= $description ?></p>
+                    <p class="card-text"><small class="text-body-secondary"><?= $date ?></small></p>
                 </div>
             </div>
         </div>
