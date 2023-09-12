4733d23c4a94f738e5f6ca19954def88d1a0b295
Valentin1610 <val.sucaud@gmail.com>
2023-09-12 15:43:38 +0200

    error404

---
+++ helpers/fnFeed.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ *  
+ *   
+ *  require_once __DIR__ . '/helpers/fnFeed.php';
+ *  // Feed::load("https://www.cnet.com/rss/news/", NumberArticles::DISPLAY_6);
+ *  $url = "https://www.cnet.com/rss/news/";
+ *  $url_1 = "https://www.zdnet.fr/feeds/rss/actualites/internet/";
+ *  $url_2 = "https://www.lefigaro.fr/rss/figaro_secteur_high-tech.xml";
+ *  $url_3 = "https://services.lesechos.fr/rss/les-echos-tech-medias.xml";
+ *  $url_4 = "https://www.lemonde.fr/cultures-web/rss_full.xml";
+
+ *  $articles = fnFeed($url_3, 6);
+ *  echo "<pre>";
+ *  print_r($articles);
+ *  echo "</pre>";
+ * 
+ */
+function fnFeed(string $url, int $nbArticles)
+{
+    $articles = [];
+    try {
+
+        $feed =  simplexml_load_file($url);
+        $channel = $feed->channel;
+        $items = $feed->channel->item;
+        foreach ($items as $key => $item) {
+
+            $title = (string) $item->title;
+            $date = (string) $item->pubDate;
+            foreach ($item->children('media', true) as $k => $v) {
+                $attributes = $v->attributes();
+                $imageUrl = (string) $attributes->url;
+            }
+            $link = (string) $item->link;
+            $article = [$title, $date, $imageUrl, $link];
+            array_push($articles, $article);
+            if (count($articles) == $nbArticles) {
+                return $articles;
+            }
+        }
+    } catch (\Throwable $th) {
+        echo 'Exception reçue : ',  $th->getMessage(), "\n";
+    }
+}
---
+++ views/templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/public/assets/img/favicon.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
+    <title>Tech Feed</title>
+</head>
+<header>
+    <div class="container-fluid">
+        <div class="row pt-3">
+            <div class="col-3 d-flex align-items-center justify-content-center">
+                <i class="fa-sharp fa-solid fa-gears"></i>
+            </div>
+            <div class="col-6">
+                <img src="/public/assets/img/logo.png" alt="logo tech feed" class="img-fluid">
+            </div>
+            <div class="col-3 d-flex align-items-center justify-content-center">
+                <i class="fa-solid fa-moon"></i>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<body>
