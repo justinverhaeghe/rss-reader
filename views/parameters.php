7ce282f42e6ead076fab57696fce2f0ac6bc6324
Forzalyonnais <verhaeghe.justin@yahoo.fr>
2023-09-13 11:58:42 +0200

    parameters add

---
+++ views/parameters.php
@@ -0,0 +1,79 @@
+<div class="container-fluid" id="parametersPage">
+    <div class="row">
+        <div class="col-12 text-center pt-2">
+            <h2>Séléction des médias</h2>
+            <p>Sélectionnez 3 médias sur les 5 présents</p>
+
+            <form method="post" id="settingsForm">
+                <div class=" row">
+                    <div class="col-12 checkboxInput">
+                        <input type="checkbox" name="figaro" id="figaro">
+                        <label for="figaro">
+                            <img src="/public/assets/img/figaro.png" alt="logo figaro" class="img-fluid mb-3">
+                        </label>
+                    </div>
+                    <div class="col-12 col-md-6 checkboxInput">
+                        <input type="checkbox" name="lemonde" id="lemonde">
+                        <label for="lemonde">
+                            <img src="/public/assets/img/lemonde.png" alt="logo lemonde" class="img-fluid mb-3">
+                        </label>
+                    </div>
+                    <div class="col-12 col-md-6 checkboxInput">
+                        <input type="checkbox" name="zdnet" id="zdnet">
+                        <label for="zdnet">
+                            <img src="/public/assets/img/zdnet.png" alt="logo zdnet" class="img-fluid mb-3">
+                        </label>
+                    </div>
+                    <div class="col-12 col-md-6 checkboxInput">
+                        <input type="checkbox" name="lesechos" id="lesechos">
+                        <label for="lesechos">
+                            <img src="/public/assets/img/lesechos.png" alt="logo lesechos" class="img-fluid mb-3">
+                        </label>
+                    </div>
+                    <div class="col-12 col-md-6 checkboxInput">
+                        <input type="checkbox" name="cnet" id="cnet">
+                        <label for="cnet">
+                            <img src="/public/assets/img/cnet.png" alt="logo cnet" class="img-fluid mb-3">
+                        </label>
+                    </div>
+
+                    <div class="col-12 text-center mb-3">
+                        <h3>Nombre d'articles</h3>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" value="six" type="radio" name="nbrArticles" id="six"
+                                checked>
+                            <label class="form-check-label" for="six">
+                                6
+                            </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" value="nine" type="radio" name="nbrArticles" id="nine">
+                            <label class="form-check-label" for="nine">
+                                9
+                            </label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" value="twelve" type="radio" name="nbrArticles" id="twelve">
+                            <label class="form-check-label" for="twelve">
+                                12
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="col-12 mb-3">
+                        <button type="button" class="btn btn-outline-light">Soumettre</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <hr>
+        <div class="col-8 offset-2 text-center">
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" role="switch" id="darkmode">
+                <label class="form-check-label" for="darkmode">
+                    <h2>Dark Mode</h2>
+                </label>
+            </div>
+        </div>
+    </div>
+</div>
--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,6 +1,10 @@
 </div>
 </div>
 </div>
+<footer class="text-center p-3">
+    <p>By Alicia, Valentin et Justin &copy; 2023</p>
+    <p class="mb-3">Projet en cours de réalisation</p>
+</footer>
 
 <script src="/public/assets/js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
