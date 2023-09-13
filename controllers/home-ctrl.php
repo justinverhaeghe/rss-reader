51d455900efb8f95d505a099cf198bc062ee90fd
Forzalyonnais <verhaeghe.justin@yahoo.fr>
2023-09-13 16:11:13 +0200

    second repair

--- controllers/home-ctrl.php
+++ controllers/home-ctrl.php
@@ -1,12 +1,13 @@
 <?php
 require_once __DIR__ . '/../helpers/fnFeed.php';
 
-if(!empty($_GET['nbrArticles'])){
+if (!empty($_GET['nbrArticles'])) {
 
-    setcookie('nbrArticles', $_GET['nbrArticles'], (time() + 3600 ), '/');
+    setcookie('nbrArticles', $_GET['nbrArticles'], (time() + 3600), '/');
 }
 
 $display = $_COOKIE['nbrArticles'] ?? 6;
+$script_js = 'home';
 
 $mediaSelects = [
     '/public/assets/img/figaro.png' => 'https://www.lefigaro.fr/rss/figaro_secteur_high-tech.xml',
